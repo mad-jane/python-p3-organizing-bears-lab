e5d593aeb698c1e7557729daab3f2613184c62ac
madeline <madeline.j.dick@gmail.com>
2023-04-17 15:19:35 -0500

    passed tests

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'Brown', 'calm', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'F', 'Black', 'rambunctious', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 11, 'F', 'Brown', 'sweet', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 12, 'F', 'Brown', 'stern', False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 6, 'F', 'Black', 'charming', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 16, 'M', 'Green', 'angry', False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 8, 'F', 'Red', 'cheerful', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 3, 'M', 'Brown', 'calm', True);
+
